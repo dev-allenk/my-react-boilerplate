575f8d9a1449b2d40cc3b9c8ddd1bbffdd2dd0f9
allen <revenoir7@gmail.com>
2019-08-05 17:32:59 +0900

    feat: 개발환경 설정

---
+++ public/components/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello Allen!</h1>
+    </>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+
+  entry: './public/entry/index.js',
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/entry/index.html'
+    })
+  ]
+
+}
